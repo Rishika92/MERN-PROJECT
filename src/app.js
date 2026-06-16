7dd2654340ea2c689bd5ff07485d019318b9ec19
Rishika92 <rishikaa.7229@gmail.com>
2026-06-16 17:02:17 +0530

    Added orders collection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express=require("express");
 const customerRoutes =require('./routes/customer.route');
 const productRoutes =require('./routes/product.route');
+const orderRoutes = require('./routes/order.route');
 const app=express();
 app.use(express.json());
 app.use('/customers', customerRoutes);
 app.use('/products', productRoutes);
+
+app.use('/orders', orderRoutes);
 module.exports=app;
